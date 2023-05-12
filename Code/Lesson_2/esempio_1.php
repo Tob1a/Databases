ba08decee9b44a907f116bbdcf7cc802c0794aa2
Tobia Sacchetto <98024054+Tob1a@users.noreply.github.com>
2023-05-12 15:32:03 +0200

    modyfing files

--- Code/Lesson_2/esempio_1.php
+++ Code/Lesson_2/esempio_1.php
@@ -1,4 +1,12 @@
-<?php
-    <?php $titolo = “Titolo”; ?>
-    <h1> <?php echo $titolo; ?> </h1>
-?>
+<!DOCTYPE>
+<html lang="en">
+    <head>
+        <title>Ciaone</title>
+    </head>
+    <body>
+        <?php
+        $titolo = "";
+        $titolo = "Titolo"; ?>
+        <h1> <?php echo $titolo; ?> </h1>
+    </body>
+</html>
